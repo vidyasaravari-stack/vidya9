27a541fcbf3ca8b0d84689e4b859001651980257
vidyasaravari-stack <vidyasaravari@gmail.com>
2025-11-27 13:13:15 +0530

    Fiveth Version

--- apple2.py
+++ apple2.py
@@ -1,6 +1,7 @@
 a = int(input("Enter a number"))
 if a  >= 18:
 	print("eligible to vote")
+	print("age is more than 18")
 else:
 	print("not eligible to  vote")
 	print("need some time to eligible for voting")
